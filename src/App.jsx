54513f6c2693dddaeb50126df5cc29d8261a2b90
Alan Caruso <alan.carusoo@gmail.com>
2023-06-08 15:04:50 -0300

    text on img

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import { useState, useEffect } from "react";
 
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
-
+const CAT_RANDOM_IMAGE_URL = "https://cataas.com";
 export default function App() {
-  const [fact, setFact] = useState("lorem ipsum");
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const firstWords = fact.split(" ", 3).join(" ");
+        console.log(firstWords);
+
+        fetch(
+          `https://cataas.com/cat/says/${firstWords}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImageUrl(url);
+          });
+      });
   }, []);
 
   return (
     <main>
       <h1>Cat Fetch</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={`${CAT_RANDOM_IMAGE_URL}${imageUrl}`}
+          alt={`Image Extracted using the first words from ${fact}`}
+        />
+      )}
     </main>
   );
 }
